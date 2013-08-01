104a8a9de7b6e594a82f03e89e5662ad7d83ea10
seadams <sky_adams@brown.edu>
2013-08-01 11:04:45 -0700

    commit during a merge

--- MCMCStructureLearning/src/Scorer.java
+++ MCMCStructureLearning/src/Scorer.java
@@ -6,10 +6,10 @@ public abstract class Scorer {
 	public int[][] Data; //assumes: the last column is disease (states 0 through diseaseStates), snps are the columns, every entry is 0 or 1
 	public int M; //the number of samples
 	public int N; //the number of snps
-	private int numConfigurations; //this is the number of parental configurations.  It is only updated after a call to getParentConfigurationCounts().
+	private int NumConfigurations; //this is the number of parental configurations.  It is only updated after a call to getParentConfigurationCounts().
 	public int AlleleStates;
 	public int DiseaseStates;
-	public double alpha;
+	public double Alpha;
 	
 	public Scorer(int[][] data, int alleleStates, int diseaseStates, double a)
 	{
@@ -19,12 +19,12 @@ public abstract class Scorer {
 		N=data[0].length - 1; //we assume that each column is a SNP except the last column
 		AlleleStates = alleleStates;
 		DiseaseStates = diseaseStates;
-		alpha=a;
+		Alpha=a;
 	}
 	
 	public int getNumConfigurations()
 	{
-		return numConfigurations;
+		return NumConfigurations;
 	}
 	
 	public double getLikelihood(ArrayList<Integer> parents)
@@ -50,7 +50,7 @@ public abstract class Scorer {
 	
 	public ArrayList<TreeNode> getParentConfigurationCounts(ArrayList<Integer> parents) {
 		//this builds a tree where each level is a snp and each node is a value that snp can 
-		//be (it assumes they can only have two values), so each parent configuration is a path 
+		//be, so each parent configuration is a path 
 		//from the root to a leaf.  The leaves contain counts of how many times the parent 
 		//configuration appears for each disease state.
 		ArrayList<TreeNode> leaves = new ArrayList<TreeNode>();
@@ -90,12 +90,53 @@ public abstract class Scorer {
 				leaves.add(current);
 			}
 		}
-		numConfigurations = leaves.size();
-		if(numConfigurations == 0)
+		NumConfigurations = leaves.size();
+		if(NumConfigurations == 0)
 		{
-			numConfigurations = 1; //if there are no parents, then there is one configuration.
+			NumConfigurations = 1; //if there are no parents, then there is one configuration.
 		}
 		return leaves;
+	}
+	
+	public TreeNode getParentConfigurations(ArrayList<Integer> parents) {
+		//this builds a tree where each level is a snp and each node is a value that snp can 
+		//be, so each parent configuration is a path 
+		//from the root to a leaf.  The leaves contain counts of how many times the parent 
+		//configuration appears for each disease state.
+		//this returns the tree.  It DOES NOT set NumConfigurations!
+		TreeNode head = new TreeNode(-2, DiseaseStates, AlleleStates);
+		for(int i=0; i < M; i++)
+		{
+			TreeNode current = head;
+			for(int j=0; j < parents.size(); j++)
+			{
+				boolean foundChild = false;
+				for(int k = 0; k < current.Children.length; k++)
+				{
+					if(current.Children[k] != null && current.Children[k].value == Data[i][parents.get(j).intValue()])
+					{
+						current = current.Children[k];
+						foundChild = true;
+						break;
+					}
+				}
+				if(!foundChild)
+				{
+					for(int k = 0; k < current.Children.length; k++)
+					{
+						if(current.Children[k] == null)
+						{
+							current.Children[k] = new TreeNode(Data[i][parents.get(j).intValue()], DiseaseStates, AlleleStates);
+							current = current.Children[k];
+							break;
+						}
+					}
+				}
+			}
+			current.isLeaf = true;
+			current.diseaseCounts[Data[i][Data[0].length-1]]++;
+		}
+		return head;
 	}
 
 	abstract double score(ArrayList<Integer> parents);
